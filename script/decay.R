fa15f859f15bea39400a894d97ead827475efc15
JJ Merelo <jjmerelo@gmail.com>
2023-02-07 20:38:01 +0100

    Tratando de trazar puntos

--- script/decay.R
+++ script/decay.R
@@ -2,7 +2,7 @@
 library(ggplot2)
 data <- read.csv("data-raw/drop-objetivos.csv")
 
-ggplot(data,aes(x=X,y=X2223.estudiantes,group="2223"))+geom_line(colour='blue')+geom_line(data=data,aes(x=X,y=X2122.estudiantes,group="2122"),colour='pink')
+ggplot(data,aes(x=X,y=X2223.estudiantes,group="2223"))+geom_line(colour='blue')+geom_point()+geom_line(data=data,aes(x=X,y=X2122.estudiantes,group="2122"),colour='pink')+geom_point(data=data,aes(x=X,y=X2122.estudiantes,group="2122")
 ggplot(data,aes(x=X,y=X2223.No.superados,group="2223"))+geom_line(colour='blue')+geom_line(data=data,aes(x=X,y=X2122.No.superados,group="2122"),colour='pink')
 ggplot(data,aes(x=X,y=X2223.No.entregados,group="2223"))+geom_line(colour='blue')+geom_line(data=data,aes(x=X,y=X2122.No.entregados,group="2122"),colour='pink')
 
