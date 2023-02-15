1880afbbbb4945d7299e33a0083d9844877ff1d0
JJ Merelo <jjmerelo@gmail.com>
2023-02-15 10:35:33 +0100

    Adapta a #2 el script, genera datos nuevos

--- script/convert-from-raw.R
+++ script/convert-from-raw.R
@@ -2,19 +2,27 @@
 
 library(parsedate)
 
-data.20.21 <- read.csv("data-raw/fechas-entrega-2021-22.csv",sep=";")
+args = commandArgs(trailingOnly=TRUE)
+if (length(args)==0) {
+  data.file = "data-raw/fechas-entrega-2021-22.csv"
+} else {
+  # default output file
+  data.file = args[1]
+}
+
+date.data <- read.csv(data.file,sep=";")
 inicio <- parse_iso_8601("2021-09-13T00:00:00+02:00")
-data.20.21$Correccion <- parse_iso_8601(data.20.21$Correccion)
-data.20.21$Correccion.Semana <- as.numeric( data.20.21$Correccion - inicio, units = "weeks")
+date.data$Correccion <- parse_iso_8601(date.data$Correccion)
+date.data$Correccion.Semana <- as.numeric( date.data$Correccion - inicio, units = "weeks")
 
-data.20.21$Entrega <- parse_iso_8601(data.20.21$Entrega)
-data.20.21$Entrega.Semana <- as.numeric( data.20.21$Entrega - inicio, units = "weeks")
+date.data$Entrega <- parse_iso_8601(date.data$Entrega)
+date.data$Entrega.Semana <- as.numeric( date.data$Entrega - inicio, units = "weeks")
 
-data.20.21$superacion <- as.numeric(data.20.21$Correccion - data.20.21$Entrega, units="days");
-data.20.21$curso = "21-22"
-data.20.21 <- data.20.21[order(data.20.21$Entrega),]
-data.20.21$entregas <- seq.int(nrow(data.20.21))
-data.20.21$Entrega <- NULL
-data.20.21$Correccion <- NULL
+date.data$superacion <- as.numeric(date.data$Correccion - date.data$Entrega, units="days");
+date.data$curso = "21-22"
+date.data <- date.data[order(date.data$Entrega),]
+date.data$entregas <- seq.int(nrow(date.data))
+date.data$Entrega <- NULL
+date.data$Correccion <- NULL
 
-save(data.20.21, file="data/entregas-20-21.rda")
+save(date.data, file="data/entregas-20-21.rda")
