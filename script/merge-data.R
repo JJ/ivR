018f898f77aa4ebcbf5ffe87db7d55071914cad2
JJ Merelo <jjmerelo@gmail.com>
2023-02-15 14:22:55 +0100

    Corrigiendo los datos

--- script/merge-data.R
+++ script/merge-data.R
@@ -8,5 +8,8 @@ load("data/entregas-22-23.rda")
 date.data.2223 <- date.data
 
 all.data <- as_tibble(rbind(date.data.2122,date.data.2223))
+all.data$entrega.Dias <- all.data$Entrega.Semana*7
+all.data$Correccion.Dias <- all.data$Correccion.Semana*7
+all.data$entregas <- NULL
 
-save(all.data, file="data/all-data.rda"))
+save(all.data, file="data/all-data.rda")
