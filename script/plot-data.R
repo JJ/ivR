1d75f9e1dd8a1f4d32177e89d6c48b5f3d911528
JJ Merelo <jjmerelo@gmail.com>
2023-02-16 17:30:51 +0100

    Adds percentile calculation

--- script/plot-data.R
+++ script/plot-data.R
@@ -5,4 +5,13 @@ library(ggplot2)
 
 load("data/all-data.rda")
 
-ggplot(all.data[all.data$Objetivo == 3,], aes(x=curso,y=superacion))+geom_boxplot(notch=T)+ylim(0,30)
+ggplot(all.data, aes(x=Aprobado,y=superacion))+geom_boxplot(notch=T)+ylim(0,30)
+
+summary(all.data[ all.data$Aprobado == T,]$superacion)
+summary(all.data[ all.data$Aprobado == F,]$superacion)
+wilcox.test( all.data[ all.data$Aprobado == T,]$superacion,all.data[ all.data$Aprobado == F,]$superacion)
+
+aprobados <- all.data[ all.data$Aprobado == T,]
+aprobados$Aprobado <- NULL
+p <- c(.5,.75)
+aprobados %>% group_by(Objetivo) %>% dplyr::summarize(porcentaje=scales::percent(p),entregas = quantile(entrega.Dias,p))
