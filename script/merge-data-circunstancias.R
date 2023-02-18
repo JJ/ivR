9f85f799b6163c74660a64afcc0242a497942cc0
JJ Merelo <jjmerelo@gmail.com>
2023-02-18 11:47:04 +0100

    Incluyendo circunstancias en un año

---
+++ script/merge-data-circunstancias.R
@@ -0,0 +1,13 @@
+#!/usr/bin/env Rscript
+
+library(tidyverse)
+
+circunstancias.2122 <- read.csv("data-raw/circunstancias-evaluacion-resultados-21-22.csv")
+circunstancias.2223 <- read.csv("data-raw/circunstancias-evaluacion-resultados-22-23.csv")
+
+circunstancias.2122$Curso <- "21-22"
+circunstancias.2223$Curso <- "21-23"
+
+circunstancias <- as_tibble( rbind( circunstancias.2122, circunstancias.2223) )
+
+save(circunstancias, file="data/circunstancias.rda")
