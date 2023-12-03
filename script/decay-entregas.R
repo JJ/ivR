b4356dafe86109ad339e8835cadc7bc454c3afcb
JJ Merelo <jjmerelo@gmail.com>
2023-12-03 11:26:51 +0100

    Añade temas #10

--- script/decay-entregas.R
+++ script/decay-entregas.R
@@ -1,9 +1,12 @@
 
 library(ggplot2)
+library(ggthemes)
 
 data.21.22 <- read.csv("data-raw/drop-objetivos-21-22.csv",header=F,sep=";")
 data.22.23 <- read.csv("data-raw/drop-objetivos-22-23.csv",header=F,sep=";")
 data.23.24 <- read.csv("data-raw/drop-objetivos-23-24.csv",header=F,sep=";")
 #
-ggplot(data.21.22,aes(x=V1,y=V3,group=1,color="21-22"))+geom_line()+geom_line(data=data.22.23,aes(x=V1,y=V3,group=2,color="22-23"))+geom_line(data=data.23.24,aes(x=V1,y=V3,group=2,color="23-24"))
+ggplot(data.21.22,aes(x=V1,y=V3,group=1,color="21-22"))+geom_line()+geom_line(data=data.22.23,aes(x=V1,y=V3,group=2,color="22-23"))+geom_line(data=data.23.24,aes(x=V1,y=V3,group=2,color="23-24"))+theme_economist()
+
+ggplot(data.21.22,aes(x=V1,y=V4,group=1,color="21-22"))+geom_line()+geom_line(data=data.22.23,aes(x=V1,y=V4,group=2,color="22-23"))+geom_line(data=data.23.24,aes(x=V1,y=V4,group=2,color="23-24"))+theme_economist()
 
