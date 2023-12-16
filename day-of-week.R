ec59f5428f282d62bc365ab8e0eae5d77a857f65
JJ Merelo <jjmerelo@gmail.com>
2023-12-16 12:23:47 +0100

    Recupera #11

--- day-of-week.R
+++ day-of-week.R
@@ -5,14 +5,16 @@ data.2122$Curso <- "21-22"
 data.2223 <- read.csv("data-raw/fechas-entrega-2022-23.csv",sep=";")
 data.2223$Curso <- "22-23"
 
-# concatenate data.2122 and data.2223
 entregas.data <- rbind( data.2122,data.2223)
 
 entregas.data <- entregas.data[ entregas.data$Incompleto == "Completo",]
 entregas.data <- entregas.data[ entregas.data$Correccion != "",]
 entregas.data$Incompleto <- NULL
 
-# Create a new column that contains the day of the week equivalent to entregas.data$Correccion
 entregas.data$dow.entrega <- as.factor(weekdays(as.Date(entregas.data$Entrega)))
 entregas.data$dow.correccion <- as.factor(weekdays(as.Date(entregas.data$Correccion)))
+entregas.data$dow.entrega <- ordered( entregas.data$dow.entrega, levels=c("lunes","martes","miércoles","jueves","viernes","sábado","domingo"))
+entregas.data$dow.correccion <- ordered( entregas.data$dow.correccion, levels=c("lunes","martes","miércoles","jueves","viernes","sábado","domingo"))
 
+ggplot(entregas.data, aes(x=dow.entrega, fill=Curso)) + geom_bar() + labs(title="Distribución de entregas por día de la semana", x="Día de la semana", y="Número de entregas", fill="Curso") + theme_bw() + theme(plot.title = element_text(hjust = 0.5))
+ggplot(entregas.data, aes(x=dow.correccion, fill=Curso)) + geom_bar() + labs(title="Distribución de correcciones por día de la semana", x="Día de la semana", y="Número de correcciones", fill="Curso") + theme_bw() + theme(plot.title = element_text(hjust = 0.5))
