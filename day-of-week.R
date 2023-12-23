1db04808bbe36a2859f7af394adce00ed18b7111
JJ Merelo <jjmerelo@gmail.com>
2023-12-23 13:31:53 +0100

    Analyzing dropping week depending on objective #16

--- day-of-week.R
+++ day-of-week.R
@@ -35,7 +35,6 @@ entregas.data$Entrega.Days  <- as.Date(entregas.data$Entrega) - as.Date(entregas
 entregas.data$Entrega.Week <- as.integer(entregas.data$Entrega.Days/7) +1
 entregas.data$First.Day.Of.Week <- as.Date(entregas.data$Inicio) + (entregas.data$Entrega.Week-1)*7
 
-
 ggplot(entregas.data, aes(x=Entrega.Week, group=Curso, color=Curso)) + geom_line(stat="count") + labs(title="Distribución de entregas por semana", x="Semana", y="Número de entregas", fill="Curso") + theme_bw() + theme(plot.title = element_text(hjust = 0.5))+xlim(0,20)
 
 ggplot(entregas.data[entregas.data$Curso=="23-24",], aes(x=First.Day.Of.Week, group=Curso, color=Curso)) + geom_line(stat="count") + geom_point(stat="count") + labs(title="Distribución de entregas por semana", x="Semana", y="Número de entregas", fill="Curso") + theme_bw() + theme(plot.title = element_text(hjust = 0.5))+scale_x_date(date_breaks = "1 week", date_labels =  "%d-%m")
@@ -43,6 +42,15 @@ ggplot(entregas.data[entregas.data$Curso=="23-24",], aes(x=First.Day.Of.Week, gr
 ggplot(entregas.data[entregas.data$Curso=="22-23",], aes(x=First.Day.Of.Week, group=Curso, color=Curso)) + geom_line(stat="count") + geom_point(stat="count") + labs(title="Distribución de entregas por semana", x="Semana", y="Número de entregas", fill="Curso") + theme_bw() + theme(plot.title = element_text(hjust = 0.5))+scale_x_date(date_breaks = "1 week", date_labels =  "%d-%m")
 
 ggplot(entregas.data[entregas.data$Curso=="21-22",], aes(x=First.Day.Of.Week, group=Curso, color=Curso)) + geom_line(stat="count") + geom_point(stat="count") + labs(title="Distribución de entregas por semana", x="Semana", y="Número de entregas", fill="Curso") + theme_bw() + theme(plot.title = element_text(hjust = 0.5))+scale_x_date(date_breaks = "1 week", date_labels =  "%d-%m") + xlim(as.Date("2021-09-13"),as.Date("2022-02-28"))
+
+
+last.entrega <- entregas.data %>% group_by(Estudiante) %>% summarise(Entrega.Week=max(Entrega.Week))
+ggplot( last.entrega, aes(x=Entrega.Week)) + geom_histogram(binwidth=1) + labs(title="Distribución últimas de entregas por semana", x="Semana", y="Número de estudiantes") + theme_bw() + theme(plot.title = element_text(hjust = 0.5))
+
+# Filter from entregas.data only the students whose max Objetivo was < 5
+entregas.data[ entregas.data$Curso != "23-24",] %>% group_by(Estudiante) %>% summarise(Max.Entrega.Week=max(Entrega.Week), Max.Objetivo=max(Objetivo)) %>% filter(Max.Objetivo < 5) -> non.passing.student
+ggplot( non.passing.student, aes(x=Max.Entrega.Week,group=Max.Objetivo,fill=Max.Objetivo)) + geom_bar(stat="count") + labs(title="Distribución últimas de entregas por semana", x="Semana", y="Número de estudiantes") + theme_bw() + theme(plot.title = element_text(hjust = 0.5))+ xlim(0,20)+scale_fill_viridis_c(option="plasma")
+
 
 ggplot(entregas.data, aes(x=dow.entrega, fill=Curso)) + geom_bar() + labs(title="Distribución de entregas por día de la semana", x="Día de la semana", y="Número de entregas", fill="Curso") + theme_bw() + theme(plot.title = element_text(hjust = 0.5))
 ggplot(entregas.data, aes(x=dow.correccion, fill=Curso)) + geom_bar() + labs(title="Distribución de correcciones por día de la semana", x="Día de la semana", y="Número de correcciones", fill="Curso") + theme_bw() + theme(plot.title = element_text(hjust = 0.5))
