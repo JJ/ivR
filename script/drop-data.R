8ccd838510191c66b0a79bc1575c8408497d44b0
JJ Merelo <jjmerelo@gmail.com>
2024-04-25 14:38:46 +0200

    Making a finer show of the data on failures

--- script/drop-data.R
+++ script/drop-data.R
@@ -19,15 +19,17 @@ objetivo.semana$Max.Objetivo <- as.factor(objetivo.semana$Max.Objetivo)
 objetivo.semana$Superado <- !is.na(objetivo.semana$Superacion)
 objetivo.semana$Estudiante <- NULL
 
-ggplot(objetivo.semana, aes(x=Max.Objetivo,y=Entrega.Semana,color=Curso,shape=Superado))+geom_jitter(size=3,width = 0.25,alpha=0.75)+theme_economist()+ylim(0,25)
+ggplot(objetivo.semana, aes(x=Max.Objetivo,y=Entrega.Semana,color=Curso,shape=Superado))+geom_jitter(size=3,width = 0.25,alpha=0.5)+theme_economist()+ylim(0,25)
 
 
 objetivo.semana %>% group_by(Max.Objetivo) %>% summarise(n = n()) %>% mutate(freq = n / sum(n)) %>% mutate( accumulated.frequency = cumsum(freq)) -> drop.objetivo
 ggplot(drop.objetivo, aes(x=Max.Objetivo,y=accumulated.frequency,group=0))+geom_point()+geom_line()+theme_economist()+ylim(0,1)
 
-objetivo.semana %>% group_by(Curso,Max.Objetivo) %>% summarise(n = n()) %>% mutate(freq = n / sum(n)) %>% mutate( accumulated.frequency = cumsum(freq)) -> drop.objetivo.curso
+objetivo.semana %>% group_by(Curso,Max.Objetivo) %>% summarise(n = n()) %>% mutate(accumulated.number=cumsum(n) ) %>% mutate(freq = n / sum(n)) %>% mutate( accumulated.frequency = cumsum(freq)) -> drop.objetivo.curso
 ggplot(drop.objetivo.curso, aes(x=Max.Objetivo,y=accumulated.frequency,group=Curso, color=Curso))+geom_point()+geom_line()+theme_economist()+ylim(0,1)
+ggplot(drop.objetivo.curso, aes(x=Max.Objetivo,y=accumulated.number,group=Curso, color=Curso))+geom_point()+geom_line()+theme_tufte()
 
-
+drop.objetivo.curso$Max.Objetivo <- as.numeric(as.character(drop.objetivo.curso$Max.Objetivo))
+drop.objetivo.curso %>% group_by(Curso) %>% filter(Max.Objetivo < 5) %>% summarise(sum.n = sum(n)) -> sum.n.curso
 
 
