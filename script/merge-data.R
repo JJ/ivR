5476c48495207c424c3e41c16b58b025357b656c
JJ Merelo <jjmerelo@gmail.com>
2024-09-30 18:23:23 +0200

    Ahora realmente tiene todos los datos

--- script/merge-data.R
+++ script/merge-data.R
@@ -6,8 +6,10 @@ load("data/entregas-21-22.rda")
 date.data.2122 <- date.data
 load("data/entregas-22-23.rda")
 date.data.2223 <- date.data
+load("data/entregas-23-24.rda")
+date.data.2324 <- date.data
 
-all.data <- as_tibble(rbind(date.data.2122,date.data.2223))
+all.data <- as_tibble(rbind(date.data.2122,date.data.2223,date.data.2324))
 all.data$entrega.Dias <- all.data$Entrega.Semana*7
 all.data$Correccion.Dias <- all.data$Correccion.Semana*7
 all.data$entregas <- NULL
